c8af0269c7c5dbbe60c4c0bfb44cb738b41f9bd9
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-05-01 08:27:01 +0900

    added dotenv dep and weather API key

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 import * as Location from 'expo-location'
+import {WEATHER_API_KEY} from '@env'
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 export default function App() {
-
   const [loading, setLoading] = useState(false)
-  const [location, setLocation]= useState(null)
+  const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+
   if (loading) {
     return (
       <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'}/>
+        <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
   }
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       let { status } = await Location.requestForegroundPermissionsAsync()
       if (status !== 'granted') {
         setError('Permission to access location was denied.')
@@ -45,13 +48,13 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-      <Tabs/>
+      <Tabs />
     </NavigationContainer>
   )
 }
 const styles = StyleSheet.create({
-  container:{
-    justifyContent:'center',
-    flex:1
+  container: {
+    justifyContent: 'center',
+    flex: 1
   }
 })
