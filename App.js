f8fbcc0ce328f86ecf06a2a1970770b45c21b9a3
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-04-30 23:25:24 +0900

    added location dep

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
 
 export default function App() {
 
   const [loading, setLoading] = useState(false)
+  const [location, setLocation]= useState(null)
+  const [error, setError] = useState(null)
   if (loading) {
     return (
       <View style={styles.container}>
         <ActivityIndicator size={'large'} color={'blue'}/>
       </View>
     )
+  }
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('Permission to access location was denied.')
+        return
+      }
+      try {
+        let location = await Location.getCurrentPositionAsync({})
+        setLocation(location)
+      } catch (error) {
+        setError('Could not get the current location.')
+      }
+    })()
+  }, [])
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>{error}</Text>
+      </View>
+    )
+  }
+
+  if (location) {
+    console.log(location)
   }
   return (
     <NavigationContainer>
