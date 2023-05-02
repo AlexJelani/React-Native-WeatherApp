cb86b22782002cc74bf9bdd7ba4404e7e0139f27
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-05-03 08:58:46 +0900

    added moment lib to upcoming weather

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,37 +1,44 @@
-import React from "react"
-import { View, Text, StyleSheet } from "react-native"
-import Feather from "react-native-vector-icons/Feather"
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Feather from 'react-native-vector-icons/Feather'
+import { weatherType } from '../utilities/weatherType'
+import moment from 'moment'
 
 const ListItem = (props) => {
-    const { dt_txt, min, max, condition } = props
-    const {item, date, temp} = styles
-    return (
-      <View style={item}>
-        <Feather name={'sun'} size={50} color={'white'} />
-        <Text style={date}>{dt_txt}</Text>
-        <Text style={temp}>{min}</Text>
-        <Text style={temp}>{max}</Text>
+  const { dt_txt, min, max, condition } = props
+  const { item, date, temp, dateTextWrapper } = styles
+  return (
+    <View style={item}>
+      <Feather name={weatherType[condition].icon} size={50} color={'white'} />
+      <View styles={styles.dateTextWrapper}>
+        <Text style={date}>{moment(dt_txt).format('dddd')}</Text>
+        <Text style={date}>{moment(dt_txt).format('h:mm:ss a')}</Text>
       </View>
-    )
+      <Text style={temp}>{`${Math.round(min)} / ${Math.round(max)}°`}</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 5,
+    backgroundColor: 'pink'
+  },
+  temp: {
+    fontSize: 20,
+    color: 'white'
+  },
+  date: {
+    fontSize: 15,
+    color: 'white'
+  },
+  dateTextWrapper:{
+    flexDirection: 'column',
   }
-  const styles = StyleSheet.create({ 
-    item: {
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderWidth: 5,
-        backgroundColor: 'pink'
-      },
-      temp: {
-        fontSize: 20,
-        color: 'white'
-      },
-      date: {
-        fontSize: 15,
-        color: 'white'
-      },
-  })
-  export default ListItem
+})
+export default ListItem
--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -2,52 +2,14 @@ import React from 'react'
 import {
   SafeAreaView,
   StyleSheet,
-  Text,
   FlatList,
   StatusBar,
   ImageBackground
 } from 'react-native'
 import ListItem from '../components/ListItem'
-const DATA = [
-  {
-    dt_txt: '2023-02-16 18:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55
-    },
-    weather: [
-      {
-        main: 'Clouds'
-      }
-    ]
-  },
-  {
-    dt_txt: '2023-02-17 18:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55
-    },
-    weather: [
-      {
-        main: 'Rain'
-      }
-    ]
-  },
-  {
-    dt_txt: '2023-02-18 18:00:00',
-    main: {
-      temp_max: 8.55,
-      temp_min: 7.55
-    },
-    weather: [
-      {
-        main: 'Clouds'
-      }
-    ]
-  }
-]
 
-const UpcomingWeather = () => {
+
+const UpcomingWeather = ({weatherData}) => {
   const renderItem = ({ item }) => {
     return (
       <ListItem
@@ -66,7 +28,7 @@ const UpcomingWeather = () => {
         style={image}
       >
         <FlatList
-          data={DATA}
+          data={weatherData}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
         />
